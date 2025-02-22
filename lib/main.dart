e21fe6b5e0cf6c1e8bc0f46b450ef92d0dfe3e94
Nahid1828 <isam15-5334@diu.edu.bd>
2025-02-22 12:32:11 +0600

    Bottom_Navigation_Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:cached_network_image/cached_network_image.dart';
+//import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,27 +11,50 @@ class Lab06AdvancedUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Lab O6 Advanced UI",
+      title: "Lab - 06",
       home: Scaffold(
-        appBar: AppBar(title: Text("Lab O6 Advanced UI")),
-        body: Center(
-          child: Column(
+        drawer: Drawer(
+          backgroundColor: Colors.grey,
+          child: ListView(
             children: [
-              CachedNetworkImage(
-                imageUrl:
-                    "https://cdn.prod.website-files.com/61845f7929f5aa517ebab941/6440f9477c2a321f0dd6ab61_How%20Artificial%20Intelligence%20(AI)%20Is%20Used%20In%20Biometrics.jpg",
-                placeholder: (context, url) => CircularProgressIndicator(),
-                errorWidget: (context, url, error) => Image.network(
-                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmWru8q17zpOzzzT1s475ZS_8fOL1GS0teSw&s'),
-              ),
-              Expanded(
-                child: Image(
-                  image: NetworkImage(
-                      'https://fastly.picsum.photos/id/337/536/354.jpg?hmac=T7nJ4MjfcTPFuaaLRH_1zwqyOFzmldcV-f5abNr2MyE'),
+              DrawerHeader(
+                child: Row(
+                  children: [Icon(Icons.people_alt_rounded), Text("Contacts")],
                 ),
-              )
+              ),
+              ListTile(
+                leading: Icon(Icons.mobile_screen_share),
+                title: Text('Device'),
+                trailing: Text("Anything"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.mobile_screen_share),
+                title: Text('Device'),
+                trailing: Text("Anything"),
+                onTap: () {},
+              ),
             ],
           ),
+        ),
+        appBar: AppBar(
+          title: const Text('Lab06 Advanced UI'),
+          backgroundColor: Colors.amber,
+        ),
+        body: Center(
+          child: Text(
+            "Working with Navigation",
+            style: TextStyle(fontSize: 60),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: "Settings",
+            ),
+          ],
         ),
       ),
     );
